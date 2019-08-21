eed95caeb86d5ed38cfbabe29279739fd1e18e52
Nixolay <i.pro.linux@yandex.ru>
2019-08-21 09:35:37 +0500

    update descriptions in datatypes.go

--- datatypes.go
+++ datatypes.go
@@ -42,13 +42,13 @@ type NotificationEvent struct {
 	EventMessage   EventInfo `json:"event_message"`
 }
 
-// EventInfo used to create messages.
+// EventInfo is used to create messages.
 type EventInfo struct {
 	Info     *MaintenanceInfo `json:"info,omitempty"`
 	Interval *int64           `json:"interval,omitempty"`
 }
 
-// CreateMessage - creates a message based EventInfo
+// CreateMessage - creates a message based on EventInfo
 func (event *NotificationEvent) CreateMessage(loc *time.Location) string {
 	if len(UseString(event.Message)) > 0 {
 		return *event.Message
